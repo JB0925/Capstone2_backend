efe60370c432198d055d1707ffef3b764e78352f
Jesse Brink <jbrink0925@gmail.com>
2024-07-13 18:12:07 -0400

    Remove duplicate PORT variable

--- server.js
+++ server.js
@@ -41,7 +41,6 @@ app.use('/api/users', userRoutes);
 app.use('/api/users', require('./routes/userRoutes'));
 app.use('/api/recipes', require('./routes/recipeRoutes'));
 
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
